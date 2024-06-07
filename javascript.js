b0ffbe2468ade377581f8a7915e2b1f0cf0043bc
Luca Ducceschi <128838177+LvcaD@users.noreply.github.com>
2024-06-07 09:34:18 +0100

    Write the logic to play a single round

--- javascript.js
+++ javascript.js
@@ -8,9 +8,6 @@ function getComputerChoice() {
         return 'Scissors'
     }
 }
-console.log(getComputerChoice())
-
-
 
 function getHumanChoice() {
     let humanInput = prompt('Rock, Paper or Scissors?')
@@ -20,11 +17,11 @@ function getHumanChoice() {
         break;
 
         case 'paper':
-            return'Paper';
+            return 'Paper';
         break;
 
         case 'scissors':
-            return'Scissors';
+            return 'Scissors';
         break;
 
         default:
@@ -33,4 +30,33 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice())
+let humanScore = 0
+let computerScore = 0
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return console.log("It's a tie!")
+    } else if (humanChoice == 'Rock' && computerChoice == 'Paper') {
+        return console.log("You lose! Paper beats Rock.")
+    } else if (humanChoice == 'Rock' && computerChoice == 'Scissors') {
+        return console.log("You win! Rock beats Scissors.")
+    } else if (humanChoice == 'Paper' && computerChoice == 'Rock') {
+        return console.log("You win! Paper beats Rock.")
+    } else if (humanChoice == 'Paper' && computerChoice == 'Scissors') {
+        return console.log("You lose! Scissors beat Paper.")
+    } else if (humanChoice == 'Scissors' && computerChoice == 'Rock') {
+        return console.log("You lose! Rock beats Scissors.")
+    } else if (humanChoice == 'Scissors' && computerChoice == 'Paper') {
+        return console.log("You win! Scissors beat Paper.")
+    } else {
+        return console.log("Error")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Computer selected: " + computerSelection)
+console.log("You selected: " + humanSelection)
+
+playRound(humanSelection, computerSelection)
